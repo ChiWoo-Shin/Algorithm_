391bcd48b5a7d5e7412e283027e9197bb3ff33d8
ChiWoo-Shin <shin8037@naver.com>
2023-02-25 23:26:57 +0900

    7장 논리연산자까지

---
+++ DeepDive/예제test.js
@@ -0,0 +1,31 @@
+// const arr = [1, 2, 3];
+
+// arr.forEach(console.log);
+//------------------------------------
+
+// console.log(score); // undefined 출력
+
+// var score;
+// score = 80;
+
+//------------------------------------
+
+// console.log(score); // undefined 출력
+
+// var score = 80;
+
+//------------------------------------
+
+console.log(score); // undefined 출력
+
+score = 80;
+var score;
+
+console.log(score); // 80 출력
+
+var str = 'My name is ';
+console.log(str)
+str +='Lee';
+console.log(str) // My name is Lee
+str -='Lee'
+console.log(str) // NaN
