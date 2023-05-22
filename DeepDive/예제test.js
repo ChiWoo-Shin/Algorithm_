5c27de7aa96b1683ae33b1d72e9f63f0a20e8ae1
ChiWoo-Shin <shin8037@naver.com>
2023-05-22 23:42:23 +0900

    25.7 끝

--- DeepDive/예제test.js
+++ DeepDive/예제test.js
@@ -4277,13 +4277,319 @@ a: a
 // ----------------------------------------------------------------------
 
 // 예제 25-33 ----------------------------------------------------------
-class Person {
-  constructor(name) {
-    // 인스턴스 프로퍼티
-    this.name = name;
+// class Person {
+//   constructor(name) {
+//     // 인스턴스 프로퍼티
+//     this.name = name;
+//   }
+// }
+
+// const me = new Person('Lee');
+// console.log(me); // Person {name: "Lee"}
+// ----------------------------------------------------------------------
+
+// 예제 25-34 ----------------------------------------------------------
+// class Person {
+//   constructor(name) {
+//     // 인스턴스 프로퍼티
+//     this.name = name; // name 프로퍼티는 public 하다
+//   }
+// }
+
+// const me = new Person('Lee');
+// console.log(me.name); // "Lee"
+// ----------------------------------------------------------------------
+
+// 예제 25-35 ----------------------------------------------------------
+// const person = {
+//   // 데이터 프로퍼티
+//   firstName: 'Ungmo',
+//   lastName: 'Lee',
+
+//   // fullName은 접근자 함수로 구성된 접근자 프로퍼티다
+//   // getter 함수
+//   get fullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+
+//   // setter 함수
+//   set fullName(name) {
+//     // 배열 디스트럭처링 할당: "36.1. 배열 디스트럭처링 할당"참고
+//     [this.firstName, this.lastName] = name.split(' ');
+//   }
+// };
+
+// // 데이터 프로퍼티를 통한 프로퍼티 값의 참조
+// console.log(`${person.firstName} ${person.lastName}`); // Ungmo Lee
+
+// // 접근자 프로퍼티를 통한 프로퍼티 값의 저장
+// // 접근자 프로퍼티 fullName에 값을 저장하면 setter 함수가 호출된다
+// person.fullName = 'Heegun Lee';
+// console.log(person); // { firstName: 'Heegun', lastName: 'Lee', fullName: [Getter/Setter] }
+
+// // 접근자 프로퍼티를 통한 프로퍼티 값의 참조
+// // 접근자 프로퍼티 fullName에 접근하면 getter 함수가 호출됨
+// console.log(person.fullName); //Heegun Lee
+
+// // fullName은 접근자 프로퍼티다.
+// // 접근자 프로퍼티는 get, set, enumerable, configurable 프로퍼티 어트리뷰트를 갖는다.
+// console.log(Object.getOwnPropertyDescriptor(person, 'fullName'));
+// // {
+// //   get: [Function: get fullName],
+// //   set: [Function: set fullName],
+// //   enumerable: true,
+// //   configurable: true
+// // }
+// ----------------------------------------------------------------------
+
+// 예제 25-36 ----------------------------------------------------------
+// class Person {
+//   constructor(firstName, lastName){
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+
+//   // fullName은 접근자 함수로 구성된 접근자 프로퍼티
+//   // getter 함수
+//   get fullName() {
+//     return `${this.firstName} ${this.lastName}`
+//   }
+
+//   set fullName(name) {
+//     [this.firstName, this.lastName] = name.split(' ');
+//   }
+// }
+
+// const me = new Person('Ungmo', 'Lee');
+
+// // 데이터 프로퍼티를 통한 프로퍼티 값의 참조
+// console.log(`${me.firstName} ${me.lastName}`); // Ungmo Lee
+
+// // 접근자 프로퍼티를 통한 프로퍼티 값의 저장
+// // 접근자 프로퍼티 fullName에 값을 저장하면 setter 함수가 호출
+// me.fullName = 'Heegun Lee';
+// console.log(me); // Person { firstName: 'Heegun', lastName: 'Lee' }
+
+// // 접근자 프로퍼티를 통한 프로퍼티 값의 참조
+// // 접근자 프로퍼티 fullName에 접근하며 getter 함수가 호출
+// console.log(me.fullName); // Heegun Lee
+
+// // fullName은 접근자 프로퍼티.
+// // 접근자 프로퍼티는 get, set, enumerable, configurable 프로퍼티 어트리뷰트를 갖음
+// console.log(Object.getOwnPropertyDescriptor(Person.prototype, 'fullName'));
+// // {
+// //   get: [Function: get fullName],
+// //   set: [Function: set fullName],
+// //   enumerable: false,
+// //   configurable: true
+// // }
+
+// // 예제 25-37 ----------------------------------------------------------
+// // Object.getOwnPropertyNames 는 비열거형(non-enumerable)을 포함한 모든 프로퍼티의 이름을 반환한다 (상속 제외)
+// Object.getOwnPropertyNames(me);
+// console.log(Object.getOwnPropertyNames(me)); // [ 'firstName', 'lastName' ]
+// Object.getOwnPropertyNames(Object.getPrototypeOf(me));
+// console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(me))); // [ 'constructor', 'fullName' ]
+// ----------------------------------------------------------------------
+
+// 예제 25-38 (Java) ----------------------------------------------------------
+// 자바 클래스의 정의
+// public class Person {
+//   // 1. 클래스 필드 정의
+//   // 클래스 필드는 클래스 몸체에 this 없이 선언해야 함
+//   private String firstName = "";
+//   private String lastName = "";
+
+//   //  생성자
+//   Person(String firstName, String lastName) {
+//     // 3. this는 언제나 클래스가 생성할 인스턴스를 가리킴
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+
+//   public String getFullName() {
+//     // 2. 클래스 필드 참조
+//     // this 없이도 클래스 필드를 참조할 수 있다
+//     return firstName + " " + this.lastName;
+//   }
+// }
+// ----------------------------------------------------------------------
+
+// 예제 25-39 -----------------------------------------------------------
+// class Person {
+//   // 클래스 필드 정의
+//   name = 'Lee';
+// }
+
+// const me = new Person('Lee');
+// ----------------------------------------------------------------------
+
+// 예제 25-40 -----------------------------------------------------------
+// class Person {
+//   // 클래스 필드 정의
+//   name = 'Lee';
+// }
+
+// const me = new Person();
+// console.log(me); // Person { name: 'Lee' }
+// ----------------------------------------------------------------------
+
+// 예제 25-41 -----------------------------------------------------------
+// class Person {
+//   // this에 클래스 필드를 바인딩해서는 안된다
+//   this.name = ''; // SyntaxError: Unexpected token '.'
+// }
+// this를 쓰고 싶으면 constructor를 쓰세요
+// ----------------------------------------------------------------------
+
+// 예제 25-42 -----------------------------------------------------------
+// class Person {
+//   // 클래스 필드
+//   name = 'Lee';
+
+//   constructor() {
+//     console.log(name); // ReferenceError: name is not defined
+//   }
+// }
+
+// new Person();
+// ----------------------------------------------------------------------
+
+// 예제 25-43 -----------------------------------------------------------
+// class Person {
+//   // 클래스 필드를 초기화하지 않으면 undefined를 갖는다
+//   name;
+// }
+
+// const me = new Person();
+// console.log(me); // Person { name: undefined }
+// ----------------------------------------------------------------------
+
+// 예제 25-44 -----------------------------------------------------------
+// class Person {
+//   name;
+
+//   constructor(name) {
+//     // 클래스 필드 초기화
+//     this.name = name;
+//   }
+// }
+
+// const me = new Person('Lee');
+// console.log(me); // Person { name: 'Lee' }
+// ----------------------------------------------------------------------
+
+// 예제 25-45 -----------------------------------------------------------
+// class Person {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// const me = new Person('Lee');
+// console.log(me); // Person { name: 'Lee' }
+// ----------------------------------------------------------------------
+
+// 예제 25-46 -----------------------------------------------------------
+// class Person {
+//   // 클래스 필드에 문자열을 할당
+//   name = 'Lee';
+
+//   // 클래스 필드에 함수를 할당
+//   getName = function () {
+//     return this.name;
+//   }
+//   // 화살표 함수로 정의할 수도 있다
+//   // getName = () => this.name;
+// }
+
+// const me = new Person();
+// console.log(me); // Person { name: 'Lee', getName: [Function: getName] }
+// console.log(me.getName()); // Lee
+// ----------------------------------------------------------------------
+
+// 예제 25-48 -----------------------------------------------------------
+// class Person {
+//   constructor(name) {
+//     this.name = name; // 인스턴스 프로퍼티는 기본적으로 public 하다
+//   }
+// }
+
+// const me = new Person('Lee');
+// console.log(me.name); // Lee
+// ----------------------------------------------------------------------
+
+// 예제 25-49 -----------------------------------------------------------
+// class Person {
+//   name = 'Lee'; // 클래스 필드도 기본적으로 public 하다
+// }
+
+// // 인스턴스 생성
+// const me = new Person('Lee');
+// console.log(me.name); // Lee
+// ----------------------------------------------------------------------
+
+// 예제 25-50 -----------------------------------------------------------
+// class Person {
+//   // private 필드 정의
+//   #name = '';
+
+//   constructor(name) {
+//     // private 필드 참조
+//     this.#name = name;
+//   }
+// }
+
+// const me = new Person('Lee');
+
+// // private 필드 #name은 클래스 외부에서 참조할 수 없다
+// console.log(me.#name); // SyntaxError: Private field '#name' must be declared in an enclosing class
+// ----------------------------------------------------------------------
+
+// 예제 25-51 -----------------------------------------------------------
+// class Person {
+//   // private 필드 정의
+//   #name = '';
+
+//   constructor(name) {
+//     this.#name = name;
+//   }
+
+//   // name은 접근자 프로퍼티다
+//   get name() {
+//     // private 필드를 참조하여 trim한 다음 반환
+//     return this.#name.trim(); // trim : 좌우열 공백 제거
+//   }
+// }
+
+// const me = new Person('  Lee  ');
+// console.log(me.name); // Lee
+// ----------------------------------------------------------------------
+
+// 예제 25-52 -----------------------------------------------------------
+// class Person {
+//   constructor(name) {
+//     // private 필드는 클래스 몸체에서 정의해야한다
+//     this.#name = name;
+//     // SyntaxError: Private field '#name' must be declared in an enclosing class
+//   }
+// }
+// ----------------------------------------------------------------------
+
+// 예제 25-53 -----------------------------------------------------------
+class MyMath {
+  // static public 필드 정의
+  static PI = 22/7;
+
+  // static private 필드 정의
+  static #num = 10;
+
+  // static 메서드
+  static increment() {
+    return ++MyMath.#num;
   }
 }
 
-const me = new Person('Lee');
-console.log(me); // Person {name: "Lee"}
+console.log(MyMath.PI); // 3.142857142857143
+console.log(MyMath.increment()); // 11
 // ----------------------------------------------------------------------
